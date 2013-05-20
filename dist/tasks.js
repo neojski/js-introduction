3b5bafa804941063f307e2c87977699b519c0d12
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2013-05-20 15:52:30 +0200

    Update tasks.js

--- dist/tasks.js
+++ dist/tasks.js
@@ -846,6 +846,38 @@ tasks.add("sum-zero", {
 
 tasks.module("homework");
 
+tasks.add("phi", {
+  instruction: "Write a function phi which for given `n >= 1` returns `phi(n)=#{1<=k<=n : gcd(k,n)=1}`.",
+  js: "var phi = function(n) {\n\n}",
+  tests: function() {
+    QUnit.test('test', function(){
+      QUnit.notStrictEqual(phi, undefined, 'phi is defined');
+      QUnit.strictEqual(typeof phi, 'function', 'phi is a function');
+      var phiref = function(n) {
+        var res = n, sq = Math.sqrt(n)+1;
+        for (var k=2; k<=sq; k++) {
+          if (n%k === 0) {
+            res /= k;
+            res *= (k-1);
+          }
+          while (n%k === 0) n /= k;
+          if (n == 1) break;
+        }
+        if (n != 1) {
+          res /= n;
+          res *= (n-1);
+        }
+        return res;
+      };
+      for (var i=1; i<=30; i++) QUnit.strictEqual(phi(i), phiref(i), 'correct value for phi(' + i + ')');
+      var t = [131072, 100140049, 1448655, 30233088, 35026003, 100000007];
+      for (var i=0; i<t.length; i++) QUnit.strictEqual(phi(t[i]), phiref(t[i]), 'correct value for phi(' + t[i] + ')');
+    });
+  }
+});
+
+tasks.module("homework");
+
 tasks.add("fibo-numbers", {
   instruction: "Write a function `f` with takes arguments `n` and `p` and returns `{fn: n-th fibonacci number (f0 = 0, f1 = 1), sum: sum of all fibonacci numbers less or equal fn modulo p}`. For incorrect arument return `{-1, -1}`.",
   js: "var f = function(n,p){\n\n}",
