e5cf57658dc6cb760060f00e5b70ba124ec8667b
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2013-05-23 17:45:16 +0200

    Update tasks.js

--- dist/tasks.js
+++ dist/tasks.js
@@ -859,6 +859,32 @@ tasks.add("sum-zero", {
 
 tasks.module("homework");
 
+tasks.add("function-composition", {
+  instruction: "Write a function compose(f, g) which, for given functions f:Y->Z, g:X->Y, returns function h=f∘g. You can assume that g takes only one argument.",
+  js: "var compose = function(f, g){\n\n}",
+  tests: function(){
+    QUnit.test('test compose', function(){
+      // for testing functions see: http://api.qunitjs.com/category/assert/
+      QUnit.notStrictEqual(compose, undefined, 'compose is defined');
+      QUnit.strictEqual(typeof compose, 'function', 'compose is a function');
+
+      var rev = function(s){ return s.split("").reverse().join(""); };
+      var doub = function(s){ return s+s; };
+      var inc = function(x){ return x+1; };
+
+      QUnit.strictEqual(typeof compose(rev, rev), 'function', 'compose returns a function');
+
+      QUnit.strictEqual(compose(doub, doub)(2), 8, 'correct value for 2*x, 2*x');
+      QUnit.strictEqual(compose(rev, rev)('wut'), 'wut', 'correct value for reverse(x), reverse(x)');
+      QUnit.strictEqual(compose(doub, rev)('abc'), 'cbacba', 'correct value for x+x, reverse(x)');
+      QUnit.strictEqual(compose(inc, doub)(4), 9, 'correct value for x+1, 2*x');
+      QUnit.strictEqual(compose(doub, inc)(4), 10, 'correct value for 2*x, x+1');
+    });
+  }
+});
+
+tasks.module("homework");
+
 tasks.add("phi", {
   instruction: "Write a function phi which for given `n >= 1` returns `phi(n)=#{1<=k<=n : gcd(k,n)=1}`.",
   js: "var phi = function(n) {\n\n}",
